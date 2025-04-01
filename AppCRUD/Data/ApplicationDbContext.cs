507d2fd27f1bbd9bfc7013e08d95c85f799e3638
ChristopherDeLaRosa <christopherdelarosadiaz@gmail.com>
2025-03-31 22:08:59 -0400

    Configuracion inicial del proyecto ASP.NET MVC, con Entity Framework

---
+++ AppCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AppCRUD.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- AppCRUD/obj/Debug/net8.0/AppCRUD.AssemblyInfo.cs
+++ AppCRUD/obj/Debug/net8.0/AppCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a334c43028c5d4d9bac9bf20cdf5e1d76f3a1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed2b6aa14668c8937cbbe6916199729aa190477")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
